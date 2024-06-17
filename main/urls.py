db3e08f8cc837777d677f8b1e61e77ecd8204635
Monavixx <dperelygin0@gmail.com>
2024-06-17 22:56:31 +0300

    login

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import information_about_api
+from django.urls import path, include
+from .views import information_about_api, LoginView
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+
 
 urlpatterns = [
-    path('', information_about_api, name='information_about_api')
+    path('info/', information_about_api, name='information_about_api'),
+    path('', include(router.urls)),
+    path('login/', LoginView.as_view(), name='login')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,49 @@
 from django.http import HttpResponse
+from rest_framework import views, response, status, permissions
+from django.contrib.auth import authenticate, login, logout
+from .serializers import UserEmailAndNameSerializer
+from rest_framework.response import Response
+from rest_framework.request import Request
+from django.contrib.auth.models import User
+import logging
+logger = logging.getLogger("mylogger")
 
 def information_about_api(request):
     #todo: json
     return HttpResponse('version: 0.0.1')
+
+class LoginView(views.APIView):
+    def _error_response(self, message):
+        return Response({
+            'success': False,
+            'message': message
+        })
+    def post(self, request: Request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        
+        #logger.info(User.objects.get(email=email).pk)
+        logger.info('rgregrgr')
+        user = authenticate(username=username, password=password)
+        
+        if user is None:
+            return self._error_response('invalid')
+        if not user.is_active:
+            return self._error_response('disabled')
+        
+        
+        
+        login(request, user)
+        
+        data = UserEmailAndNameSerializer(request.user).data
+        data['success'] = True
+        return Response(data)
+
+    def get(self, request: Request):
+        logger.info(request.user.is_authenticated)
+        if request.user is None or not request.user.is_authenticated:
+            return self._error_response('not authenticated')
+        data = UserEmailAndNameSerializer(request.user).data
+        data['success'] = True
+        return Response(data)
