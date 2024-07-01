48404c284359205c5e9fa5ee852b849049c4006e
Monavixx <dperelygin0@gmail.com>
2024-07-01 13:19:00 +0300

    call clean method in save method in dixxuser model

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
-
+from django.core.exceptions import ValidationError
 
 
 class DiXXUserManager(UserManager):
@@ -10,3 +10,15 @@ class DiXXUser(AbstractUser):
     email = models.EmailField(unique=True)
     gender = models.CharField(max_length=1, choices={'m':'man', 'w': 'woman'}, blank=True, null=True)
     objects = DiXXUserManager()
+
+    def clean(self, *args, **kwargs):
+        super().clean(*args, **kwargs)
+
+        #BAD_SYMBOLS = ' \t\n'
+        #print(self.username)
+        #for bs in BAD_SYMBOLS:
+        #    if bs in self.username:
+        #        raise ValidationError('Username must not contains \'' + bs + '\' symbol.')
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
