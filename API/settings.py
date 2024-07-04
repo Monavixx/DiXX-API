8d98a6c9d01bc74e7f7009161c6bf3c24ec9fce7
Monavixx <dperelygin0@gmail.com>
2024-07-04 13:58:43 +0300

    fix automatical ip

--- API/settings.py
+++ API/settings.py
@@ -59,8 +59,15 @@ CORS_ALLOW_HEADERS = (
     'x-csrftoken'
 )
 
+
+#import socket
+#ip = socket.gethostbyname(socket.gethostname())
+
 import socket
-ip = socket.gethostbyname(socket.gethostname())
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip = s.getsockname()[0]
+s.close()
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
